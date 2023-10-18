ec3228de3086df0c8ae7a08f8555aed131dd3083
HelloSunPKU2023 <hellosunpku@gmail.com>
2023-10-18 23:29:02 +0800

    polish the UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ if st.button('Predict'):
         
         # # display the predictions in green, one line per prediction
         for i in range(5):
-            st.markdown(f"<font color='green'>{i+1}. {predictions[i*2]}</font>: <font color='red'>{predictions[i*2+1]*100:.2f}%</font>", unsafe_allow_html=True)
+            st.markdown(f"<font color='green'>{i+1}. {predictions[i*2]}</font>: <font color='red'>{predictions[i*2+1]*100:.4f}%</font>", unsafe_allow_html=True)
             # st.write(f"{i+1}. {prediction}")
 
         # st.write(f"Title Cleaned: {title}")
