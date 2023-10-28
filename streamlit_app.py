c28b4c9cc4ef3a3b9c22525e3188573c588aa011
HelloSunPKU2023 <hellosunpku@gmail.com>
2023-10-28 15:08:53 +0800

    refetoring

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,9 +36,13 @@ vectorizer3 = joblib.load(f'models/vectorizer_{MODEL_3}.pkl', 'rb')
 model4 = joblib.load(f'models/model_{MODEL_4}.pkl', 'rb')
 vectorizer4 = joblib.load(f'models/vectorizer_{MODEL_4}.pkl', 'rb')
 
+MODEL_NNAMES = [MODEL_1, MODEL_2, MODEL_3, MODEL_4]
+models = [model1, model2, model3, model4]
+vectorizers = [vectorizer1, vectorizer2, vectorizer3, vectorizer4]
+
 # add a text area
 title = st.text_area('Title/Description of a SMAX Ticket (type in the box below):', '')
-
+title_cleaned = ""
 # add a disclaimer
 st.markdown(f"<font color='red' size=4 ><b>Disclaimer: For demonstration purposes only; predictions may be inaccurate.</b></font>", unsafe_allow_html=True)
 
@@ -49,68 +53,81 @@ if st.button('Predict'):
         st.write("Please enter a title or a short description of the issue.")
         st.stop()
     
-    # Model 1
-    # predict
-    df1 = predict_top5(model = model1, vectorizer = vectorizer1, X_test = [title])
-    # display the prediction
-    if df1 is None:
-        st.write(f"Cannot predict by{MODEL_1} model")
-    else:
-        predictions1 = df1.iloc[0, 1:11].tolist()
-        title = df1.iloc[0, 0]
-        # display the tile in blue and bold font and larger size
-        st.markdown(f"Title cleaned: <font color='blue' size=5 ><b>{title}</b></font>", unsafe_allow_html=True)
-        st.markdown(f"Predicted by: {MODEL_1} model")
-        # # display the predictions in green, one line per prediction
-        for i in range(top_num):
-            if predictions1[i*2+1]>0:
-                st.markdown(f"<font color='green'>{i+1}. {predictions1[i*2]}</font>: <font color='red'>{predictions1[i*2+1]*100:.4f}%</font>", unsafe_allow_html=True)
+    for i in range(len(models)):
+        df = predict_top5(model = models[i], vectorizer = vectorizers[i], X_test = [title])
+        if df is not None:
+            st.write(f"Cannot predict by {MODEL_NNAMES[i]} model")
+        else:
+            prediction = df.iloc[0, 1:11].tolist()
+            
+            if title_cleaned == "":
+                title_cleaned = df.iloc[0, 0]
+                st.markdown(f"Title cleaned: <font color='blue' size=5 ><b>{title_cleaned}</b></font>", unsafe_allow_html=True)
+            for i in range(top_num):
+                if prediction[i*2+1]>0:
+                    st.markdown(f"<font color='green'>{i+1}. {prediction[i*2]}</font>: <font color='red'>{prediction[i*2+1]*100:.4f}%</font>", unsafe_allow_html=True)
+    # # Model 1
+    # # predict
+    # df1 = predict_top5(model = model1, vectorizer = vectorizer1, X_test = [title])
+    # # display the prediction
+    # if df1 is None:
+    #     st.write(f"Cannot predict by{MODEL_1} model")
+    # else:
+    #     predictions1 = df1.iloc[0, 1:11].tolist()
+    #     title = df1.iloc[0, 0]
+    #     # display the tile in blue and bold font and larger size
+    #     st.markdown(f"Title cleaned: <font color='blue' size=5 ><b>{title}</b></font>", unsafe_allow_html=True)
+    #     st.markdown(f"Predicted by: {MODEL_1} model")
+    #     # # display the predictions in green, one line per prediction
+    #     for i in range(top_num):
+    #         if predictions1[i*2+1]>0:
+    #             st.markdown(f"<font color='green'>{i+1}. {predictions1[i*2]}</font>: <font color='red'>{predictions1[i*2+1]*100:.4f}%</font>", unsafe_allow_html=True)
     
-    # Model 2
-    st.markdown(f"Predicted by: {MODEL_2} model")
-    # predict
-    df2 = predict_top5(model = model2, vectorizer = vectorizer2, X_test = [title])
-    # display the prediction
-    if df2 is None:
-        st.write(f"Cannot predict by {MODEL_2} model")
-    else:
-        predictions2 = df2.iloc[0, 1:11].tolist()
-        if df1 is None:
-            title = df2.iloc[0, 0]
-            st.markdown(f"Title cleaned: <font color='blue' size=5 ><b>{title}</b></font>", unsafe_allow_html=True)
-        for i in range(top_num):
-            if predictions2[i*2+1]>0:
-                st.markdown(f"<font color='green'>{i+1}. {predictions2[i*2]}</font>: <font color='red'>{predictions2[i*2+1]*100:.4f}%</font>", unsafe_allow_html=True)
+    # # Model 2
+    # st.markdown(f"Predicted by: {MODEL_2} model")
+    # # predict
+    # df2 = predict_top5(model = model2, vectorizer = vectorizer2, X_test = [title])
+    # # display the prediction
+    # if df2 is None:
+    #     st.write(f"Cannot predict by {MODEL_2} model")
+    # else:
+    #     predictions2 = df2.iloc[0, 1:11].tolist()
+    #     if df1 is None:
+    #         title = df2.iloc[0, 0]
+    #         st.markdown(f"Title cleaned: <font color='blue' size=5 ><b>{title}</b></font>", unsafe_allow_html=True)
+    #     for i in range(top_num):
+    #         if predictions2[i*2+1]>0:
+    #             st.markdown(f"<font color='green'>{i+1}. {predictions2[i*2]}</font>: <font color='red'>{predictions2[i*2+1]*100:.4f}%</font>", unsafe_allow_html=True)
                 
     
-    # Model 3
-    st.markdown(f"Predicted by: {MODEL_3} model")
-    # predict
-    df3 = predict_top5(model = model3, vectorizer = vectorizer3, X_test = [title])
-    # display the prediction
-    if df3 is None:
-        st.write(f"Cannot predict by {MODEL_3} model")
-    else:
-        predictions3 = df3.iloc[0, 1:11].tolist()
-        if df1 is None:
-            title = df3.iloc[0, 0]
-            st.markdown(f"Title cleaned: <font color='blue' size=5 ><b>{title}</b></font>", unsafe_allow_html=True)
-        for i in range(top_num):
-            if predictions3[i*2+1]>0:
-                st.markdown(f"<font color='green'>{i+1}. {predictions3[i*2]}</font>: <font color='red'>{predictions3[i*2+1]*100:.4f}%</font>", unsafe_allow_html=True)
+    # # Model 3
+    # st.markdown(f"Predicted by: {MODEL_3} model")
+    # # predict
+    # df3 = predict_top5(model = model3, vectorizer = vectorizer3, X_test = [title])
+    # # display the prediction
+    # if df3 is None:
+    #     st.write(f"Cannot predict by {MODEL_3} model")
+    # else:
+    #     predictions3 = df3.iloc[0, 1:11].tolist()
+    #     if df1 is None:
+    #         title = df3.iloc[0, 0]
+    #         st.markdown(f"Title cleaned: <font color='blue' size=5 ><b>{title}</b></font>", unsafe_allow_html=True)
+    #     for i in range(top_num):
+    #         if predictions3[i*2+1]>0:
+    #             st.markdown(f"<font color='green'>{i+1}. {predictions3[i*2]}</font>: <font color='red'>{predictions3[i*2+1]*100:.4f}%</font>", unsafe_allow_html=True)
     
-    # Model 4
-    st.markdown(f"Predicted by: {MODEL_4} model")
-    # predict
-    df4 = predict_top5(model = model4, vectorizer = vectorizer4, X_test = [title])
-    # display the prediction
-    if df4 is None:
-        st.write(f"Cannot predict by {MODEL_4} model")
-    else:
-        predictions4 = df4.iloc[0, 1:11].tolist()
-        if df1 is None:
-            title = df4.iloc[0, 0]
-            st.markdown(f"Title cleaned: <font color='blue' size=5 ><b>{title}</b></font>", unsafe_allow_html=True)
-        for i in range(top_num):
-            if predictions4[i*2+1]>0:
-                st.markdown(f"<font color='green'>{i+1}. {predictions4[i*2]}</font>: <font color='red'>{predictions4[i*2+1]*100:.4f}%</font>", unsafe_allow_html=True)
+    # # Model 4
+    # st.markdown(f"Predicted by: {MODEL_4} model")
+    # # predict
+    # df4 = predict_top5(model = model4, vectorizer = vectorizer4, X_test = [title])
+    # # display the prediction
+    # if df4 is None:
+    #     st.write(f"Cannot predict by {MODEL_4} model")
+    # else:
+    #     predictions4 = df4.iloc[0, 1:11].tolist()
+    #     if df1 is None:
+    #         title = df4.iloc[0, 0]
+    #         st.markdown(f"Title cleaned: <font color='blue' size=5 ><b>{title}</b></font>", unsafe_allow_html=True)
+    #     for i in range(top_num):
+    #         if predictions4[i*2+1]>0:
+    #             st.markdown(f"<font color='green'>{i+1}. {predictions4[i*2]}</font>: <font color='red'>{predictions4[i*2+1]*100:.4f}%</font>", unsafe_allow_html=True)
