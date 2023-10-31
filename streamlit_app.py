948756d8eb705fca77528579f302b653e0f7f402
HelloSunPKU2023 <hellosunpku@gmail.com>
2023-10-31 21:44:52 +0800

    add explaination of Other Products

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,3 +89,6 @@ if st.button('Guess'):
             for i in range(top_num):
                 if prediction[i*2+1]>0:
                     st.markdown(f"<font color='green'>{i+1}. {prediction[i*2]}</font>: <font color='red'>{prediction[i*2+1]*100:.1f}%</font>", unsafe_allow_html=True)
+
+other_products = ['VISAGE', 'eSearch', 'Drillbench', 'Engine Ecosystem, Sim Cluster Mgr.', 'MEPO', 'FDPlan', 'Enterprise Data Solution', 'Flaresim', 'Developer Portal', 'Cameron Supplier Portal', 'Drilling Insights']
+st.write(f"Other Products (not in top 25): {', '.join(other_products)}") 
