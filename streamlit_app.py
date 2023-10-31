7299fd8701d41a65e01bfa123ea1100dd9fa0413
HelloSunPKU2023 <hellosunpku@gmail.com>
2023-10-31 19:38:48 +0800

    preload models for better performance

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,45 +9,79 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 from src.helper_pred import predict_top5
 """
-# Predict Product by Ticket Title
+# Guess what product are you talking about...
 
-Put a `Title/Description of a SMAX Ticket` then click the `Predict` button. \n
-There is a `>93%` chance that the correct product name is in the top 3 predictions.
+Put a `SMAX ticket Title or Description` then click the `Guess` button. \n
+There is a `+93%` chance that the correct product name is in the top 3 predictions.
 
 """
 
 # load the model
 MODEL_1 =  'voting'
+
 MODEL_2 =  'svc_linear'
+
+# Configuration: Classify top 25 products; Text column: Title_Translated, Title words length: (4, 15);
+# Records/product caped at 6000; Class weight factor: -1; class weight power: 0.5.
+# Accuracy of top 1 prediction is 0.819.
+# Accuracy of top 3 prediction is 0.951.
+# Accuracy of top 5 prediction is 0.974.
 MODEL_3 =  'logistic_regression'
+# Configuration: Classify top 25 products; Text column: Title_Translated, Title words length: (4, 15);
+# Records/product caped at 6000; Class weight factor: -1; class weight power: 0.5.
+# Accuracy of top 1 prediction is 0.828.
+# Accuracy of top 3 prediction is 0.947.
+# Accuracy of top 5 prediction is 0.972.
 MODEL_4 =  'sgc_classifier'
 
 top_num = 3
 
-model1 = joblib.load(f'models/model_{MODEL_1}.pkl', 'rb')
-vectorizer1 = joblib.load(f'models/vectorizer_{MODEL_1}.pkl', 'rb')
+@st.cache
+def load_models():
+    model1 = joblib.load(f'models/model_{MODEL_1}.pkl', 'rb')
+    vectorizer1 = joblib.load(f'models/vectorizer_{MODEL_1}.pkl', 'rb')
 
-model2 = joblib.load(f'models/model_{MODEL_2}.pkl', 'rb')
-vectorizer2 = joblib.load(f'models/vectorizer_{MODEL_2}.pkl', 'rb')
+    model2 = joblib.load(f'models/model_{MODEL_2}.pkl', 'rb')
+    vectorizer2 = joblib.load(f'models/vectorizer_{MODEL_2}.pkl', 'rb')
 
-model3 = joblib.load(f'models/model_{MODEL_3}.pkl', 'rb')
-vectorizer3 = joblib.load(f'models/vectorizer_{MODEL_3}.pkl', 'rb')
+    model3 = joblib.load(f'models/model_{MODEL_3}.pkl', 'rb')
+    vectorizer3 = joblib.load(f'models/vectorizer_{MODEL_3}.pkl', 'rb')
 
-model4 = joblib.load(f'models/model_{MODEL_4}.pkl', 'rb')
-vectorizer4 = joblib.load(f'models/vectorizer_{MODEL_4}.pkl', 'rb')
+    model4 = joblib.load(f'models/model_{MODEL_4}.pkl', 'rb')
+    vectorizer4 = joblib.load(f'models/vectorizer_{MODEL_4}.pkl', 'rb')
 
-MODEL_NNAMES = [MODEL_1, MODEL_2, MODEL_3, MODEL_4]
-models = [model1, model2, model3, model4]
-vectorizers = [vectorizer1, vectorizer2, vectorizer3, vectorizer4]
+    MODEL_NNAMES = [MODEL_1, MODEL_2, MODEL_3, MODEL_4]
+    models = [model1, model2, model3, model4]
+    vectorizers = [vectorizer1, vectorizer2, vectorizer3, vectorizer4]
+    return MODEL_NNAMES, models, vectorizers
+
+
+# model1 = joblib.load(f'models/model_{MODEL_1}.pkl', 'rb')
+# vectorizer1 = joblib.load(f'models/vectorizer_{MODEL_1}.pkl', 'rb')
+
+# model2 = joblib.load(f'models/model_{MODEL_2}.pkl', 'rb')
+# vectorizer2 = joblib.load(f'models/vectorizer_{MODEL_2}.pkl', 'rb')
+
+# model3 = joblib.load(f'models/model_{MODEL_3}.pkl', 'rb')
+# vectorizer3 = joblib.load(f'models/vectorizer_{MODEL_3}.pkl', 'rb')
+
+# model4 = joblib.load(f'models/model_{MODEL_4}.pkl', 'rb')
+# vectorizer4 = joblib.load(f'models/vectorizer_{MODEL_4}.pkl', 'rb')
+
+# MODEL_NNAMES = [MODEL_1, MODEL_2, MODEL_3, MODEL_4]
+# models = [model1, model2, model3, model4]
+# vectorizers = [vectorizer1, vectorizer2, vectorizer3, vectorizer4]
+
+load_models()
 
 # add a text area
 title = st.text_area('Type in the box below:', height=50)
 title_cleaned = ""
 # add a disclaimer
-st.markdown(f"<font color='red' size=4 ><b>Disclaimer: For demonstration purposes only; predictions may be inaccurate.</b></font>", unsafe_allow_html=True)
+st.markdown(f"<font color='red' size=3 ><b>Disclaimer: For demonstration purposes only; predictions may be inaccurate.</b></font>", unsafe_allow_html=True)
 
 # add a button
-if st.button('Predict'):
+if st.button('Guess'):
     # check if the user has entered a title
     if title.strip() == '':
         st.write("Please enter a title or a short description of the issue.")
@@ -63,7 +97,7 @@ if st.button('Predict'):
             if title_cleaned == "":
                 title_cleaned = df.iloc[0, 0]
                 st.markdown(f"Title cleaned: <font color='blue' size=5 ><b>{title_cleaned}</b></font>", unsafe_allow_html=True)
-            st.markdown(f"Predicted by: {MODEL_NNAMES[i]} model：")
+            st.markdown(f"Predicted by: {MODEL_NNAMES[i]} model")
             for i in range(top_num):
                 if prediction[i*2+1]>0:
                     st.markdown(f"<font color='green'>{i+1}. {prediction[i*2]}</font>: <font color='red'>{prediction[i*2+1]*100:.1f}%</font>", unsafe_allow_html=True)
